c83a0964be4448f28f3c89b8c60eac167475fe7f
DevinDon <DevinDon@Foxmail.com>
2019-01-11 22:04:19 +0800

    chore: update package KBS

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,37 +2,9 @@ import { Server } from 'koa-backend-server';
 import PATH from './router';
 
 const server = new Server({
-  address: {
-    portocol: 'HTTP',
-    port: 8080
-  },
-  database: {
-    options: {
-      'type': 'mysql',
-      'host': 'app-database',
-      'port': 3306,
-      'username': 'app',
-      'password': 'app',
-      'database': 'app',
-      'synchronize': true,
-      'logging': true,
-      'entities': [
-        'src/entity/**/*.entity.ts'
-      ],
-      'migrations': [],
-      'subscribers': []
-    }
-  },
   router: {
     paths: PATH,
     version: 'v1'
-  },
-  session: {
-    name: 'session.id',
-    redis: {
-      host: 'app-redis',
-      port: 6379
-    }
   }
 });
 
