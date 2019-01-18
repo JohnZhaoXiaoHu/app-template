b01bce72bacf4380c8a667ed3b5d60d17050019b
DevinDon <DevinDon@Foxmail.com>
2019-01-18 23:18:11 +0800

    fix(server): use postgres to default example

--- server/src/entity/user.entity.ts
+++ server/src/entity/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'user' })
 export class User extends BaseEntity {
@@ -6,25 +6,20 @@ export class User extends BaseEntity {
   /** User ID. */
   @PrimaryGeneratedColumn({
     type: 'bigint',
-    unsigned: true,
-    comment: 'User ID.'
+    comment: 'User ID'
   })
   id: number;
 
   /** User name. */
   @Column({
-    type: 'char',
-    length: 64,
-    nullable: false,
+    type: 'text',
     comment: 'User name'
   })
   name: string;
 
   /** User email. */
   @Column({
-    type: 'char',
-    length: 64,
-    nullable: true,
+    type: 'text',
     comment: 'User email'
   })
   email: string;
