627764796d8f95c4700282068973529c19a317e5
DevinDon <DevinDon@Foxmail.com>
2019-01-23 00:31:06 +0800

    fix(client): fix TrackByFunction import

--- client/src/app/service/app.service.ts
+++ client/src/app/service/app.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TrackByFunction } from '@angular/core';
 import { trackBy } from '../other/function';
-import { TrackByFunction } from '../other/type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  public trackByID: TrackByFunction = trackBy('id');
+  public trackByID: TrackByFunction<any> = trackBy('id');
 
   constructor() { }
 
