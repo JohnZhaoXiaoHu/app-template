39045f1d71b05b630fde5b8a26f7ba853e8e9323
DevinDon <DevinDon@Foxmail.com>
2019-01-23 16:58:02 +0800

    feat(server): add generateResponse method

---
+++ server/src/router/config.ts
@@ -0,0 +1,9 @@
+import { Response } from '../@types';
+
+export function generateResponse<T>(content: T): Response<T> {
+  return {
+    id: Date.now(),
+    status: Boolean(content),
+    content
+  };
+}
