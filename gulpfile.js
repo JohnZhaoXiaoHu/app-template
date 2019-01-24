e24029087277bf433a4e7faa0dde90637098a50a
DevinDon <DevinDon@Foxmail.com>
2019-01-24 15:48:23 +0800

    feat(docker): support arm32v7 platform

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const gulpClean = require('gulp-clean');
+const gulpConcat = require('gulp-concat');
 
 /** Source path. */
 const PATH = {
@@ -36,9 +37,29 @@ async function clean() {
 
 gulp.task('clean', clean);
 
-// Pack app to /dist folder
+// Pack app to /dist folder, x86/64 platform
 gulp.task('pack', async () => {
-  gulp.src(PATH.docker.path + '**/*')
+  gulp.src(PATH.docker.path + 'config/**/*')
+    .pipe(gulp.dest(PATH.dist + 'config'));
+  gulp.src(PATH.docker.path + 'server/Dockerfile')
+    .pipe(gulp.dest(PATH.dist + 'server'));
+  gulp.src(PATH.docker.path + 'docker-compose.yml')
+    .pipe(gulp.dest(PATH.dist));
+  gulp.src(PATH.client.dist + '**/*')
+    .pipe(gulp.dest(PATH.dist + 'client'));
+  gulp.src([PATH.server.dist + '**/*.js', PATH.server.path + '*.json'])
+    .pipe(gulp.dest(PATH.dist + 'server'));
+});
+
+// Pack app to /dist folder, arm32v7 platform
+gulp.task('pack:arm32v7', async () => {
+  gulp.src(PATH.docker.path + 'config/**/*')
+    .pipe(gulp.dest(PATH.dist + 'config'));
+  gulp.src(PATH.docker.path + 'server/Dockerfile.arm32v7')
+    .pipe(gulpConcat('Dockerfile'))
+    .pipe(gulp.dest(PATH.dist + 'server'));
+  gulp.src(PATH.docker.path + 'docker-compose.arm32v7.yml')
+    .pipe(gulpConcat('docker-compose.yml'))
     .pipe(gulp.dest(PATH.dist));
   gulp.src(PATH.client.dist + '**/*')
     .pipe(gulp.dest(PATH.dist + 'client'));
