9adc9568d2673a3a9bd0de6109b3b24469914430
DevinDon <DevinDon@Foxmail.com>
2019-01-27 00:46:02 +0800

    refactor(app.component): bind title to app service & move it to component root folder

---
+++ client/src/app/component/app.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { APPService } from 'src/app/service/app.service';
+import { APPComponent } from './app.component';
+
+describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<APPComponent>;
+  let component: APPComponent;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        APPComponent
+      ],
+      providers: [
+        APPService
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(APPComponent);
+    component = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have as title "Client"', () => {
+    expect(component.app.title).toEqual('Client');
+  });
+
+  it('should render title in a p.welcome tag', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p.welcome').textContent).toContain('Welcome to Client!');
+  });
+
+});
--- client/src/app/component/app.component.ts
+++ client/src/app/component/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { APPService } from 'src/app/service/app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'Hello, Angular!';
+export class APPComponent {
+
+  constructor(
+    public app: APPService
+  ) { }
+
 }
