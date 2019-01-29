aec864e13a531fac495176b519cade2c66bab0d3
DevinDon <DevinDon@Foxmail.com>
2019-01-29 22:58:06 +0800

    feat(dialog.component): add scss support

--- client/src/app/component/dialog/dialog-alert/dialog-alert.component.ts
+++ client/src/app/component/dialog/dialog-alert/dialog-alert.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dialog-alert',
   templateUrl: './dialog-alert.component.html',
-  styleUrls: ['./dialog-alert.component.less']
+  styleUrls: ['./dialog-alert.component.scss']
 })
 export class DialogAlertComponent implements OnInit {
 
