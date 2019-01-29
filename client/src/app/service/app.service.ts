f0cce7524ce9922d7648f3c983919fbeabdfc361
DevinDon <DevinDon@Foxmail.com>
2019-01-29 22:57:31 +0800

    feat(app.service): add theme change function

--- client/src/app/service/app.service.ts
+++ client/src/app/service/app.service.ts
@@ -13,6 +13,35 @@ export class APPService {
   /** TrackByIDFunction. */
   trackByID: TrackByFunction<any> = trackBy('id');
 
-  constructor() { }
+  private themeColor: HTMLElement;
+  private bodyCSS: DOMTokenList;
+
+  constructor() {
+    this.themeColor = document.head.querySelector('[name=theme-color]');
+    this.bodyCSS = document.body.classList;
+  }
+
+  /**
+   * Change material theme.
+   * @param {'light' | 'dark'} name Theme name.
+   * @returns {void} Void.
+   */
+  changeTheme(name?: 'light' | 'dark'): void {
+    if (name) {
+      this.bodyCSS.remove('use-light-theme', 'use-dark-theme');
+      this.bodyCSS.add(`use-${name}-theme`);
+    } else {
+      if (this.bodyCSS.contains('use-light-theme')) {
+        this.bodyCSS.remove('use-light-theme');
+        this.bodyCSS.add('use-dark-theme');
+        this.themeColor.setAttribute('content', '#00bcd4');
+      } else {
+        this.bodyCSS.remove('use-dark-theme');
+        this.bodyCSS.add('use-light-theme');
+        this.themeColor.setAttribute('content', '#009688');
+      }
+    }
+    console.log(`Theme changed.`);
+  }
 
 }
