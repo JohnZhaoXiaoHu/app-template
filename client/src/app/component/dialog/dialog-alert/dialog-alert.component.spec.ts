c1f16ff5b95d319688882d882ecccb3b2ba403a5
DevinDon <DevinDon@Foxmail.com>
2019-01-31 18:07:06 +0800

    test(dialog-alert.component): update test

--- client/src/app/component/dialog/dialog-alert/dialog-alert.component.spec.ts
+++ client/src/app/component/dialog/dialog-alert/dialog-alert.component.spec.ts
@@ -1,5 +1,10 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestingModule } from 'src/app/module/testing.module';
 import { DialogAlertComponent } from './dialog-alert.component';
+
+@Component({ selector: 'app-dialog-alert', template: '' })
+export class StubDialogAlertComponent { }
 
 describe('DialogAlertComponent', () => {
 
@@ -8,12 +13,16 @@ describe('DialogAlertComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [TestingModule],
       declarations: [DialogAlertComponent]
     }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(DialogAlertComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
