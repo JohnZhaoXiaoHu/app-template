85f34b3f0dae8d083d16805694d885b5df2172f2
DevinDon <DevinDon@Foxmail.com>
2019-01-31 19:40:31 +0800

    test(module): add testing module

---
+++ client/src/app/module/testing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import 'jasmine';
+import { StubAboutComponent } from '../component/content/about/about.component.spec';
+import { StubContentComponent } from '../component/content/content.component.spec';
+import { StubHomeComponent } from '../component/content/home/home.component.spec';
+import { StubSidenavComponent } from '../component/content/sidenav.component.spec';
+import { StubDialogAlertComponent } from '../component/dialog/dialog-alert/dialog-alert.component.spec';
+import { MatModule } from './mat.module';
+
+@NgModule({
+  declarations: [
+    StubAboutComponent,
+    StubHomeComponent,
+    StubContentComponent,
+    StubSidenavComponent,
+    StubDialogAlertComponent
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    RouterTestingModule,
+    MatModule,
+    FormsModule,
+    FlexLayoutModule,
+    StubAboutComponent,
+    StubHomeComponent,
+    StubContentComponent,
+    StubSidenavComponent,
+    StubDialogAlertComponent
+  ]
+})
+export class TestingModule { }
