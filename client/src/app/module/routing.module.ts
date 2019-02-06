729204628a2c83b9a3da3bdf2ba7b8cb6d463a5f
DevinDon <DevinDon@Foxmail.com>
2019-02-07 01:15:05 +0800

    feat(routing.module): update routing module

--- client/src/app/module/routing.module.ts
+++ client/src/app/module/routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from '../component/content/about/about.component';
+import { HomeComponent } from '../component/content/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'home', pathMatch: 'full', redirectTo: '' },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
