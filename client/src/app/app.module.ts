f2d533170ca45d61b08c47023fba6d033fe38b04
DevinDon <DevinDon@Foxmail.com>
2019-02-17 13:24:53 +0800

    feat(app.module): update module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { APPComponent } from './component/app.component';
+import { AboutComponent } from './component/content/about/about.component';
+import { ContentComponent } from './component/content/content.component';
+import { HomeComponent } from './component/content/home/home.component';
+import { SidenavComponent } from './component/content/sidenav.component';
 import { DialogModule } from './module/dialog.module';
 import { MatModule } from './module/mat.module';
 import { OtherModule } from './module/other.module';
 import { APPRoutingModule } from './module/routing.module';
 import { FillWithPipe } from './other/pipe/fill-with.pipe';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     APPComponent,
-    FillWithPipe
+    FillWithPipe,
+    ContentComponent,
+    HomeComponent,
+    AboutComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
