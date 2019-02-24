22a7155c994f9fd3d1ad4d7ff2aa4c81aaa9451e
IInfinity <DevinDon@Foxmail.com>
2019-02-24 13:51:23 +0800

    perf(docker): perf docker stack deploy

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,1 @@
-docker build -t your-app-nginx ./nginx
-docker build -t your-app-server ./server
-docker stack deploy -c docker-compose.yml your-app-name
+docker stack deploy -c docker-compose.yml app-name
