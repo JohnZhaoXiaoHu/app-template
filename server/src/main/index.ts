21e65100558e0cf06458e95a7a8fa2d2095e6aca
IInfinity <DevinDon@Foxmail.com>
2019-03-25 12:05:33 +0800

    perf(server): use code to config server

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,12 +1,96 @@
-import { Server } from 'koa-backend-server';
+import { Server, KBSConfig } from 'koa-backend-server';
 import PATH from './router';
 import { statistic } from './ware';
 
-const server = new Server({
+/** Production config. */
+const prodConfig: KBSConfig = {
+  address: {
+    portocol: 'HTTP',
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: true, // In proxy mode or not
+    ssl: {
+      cert: 'CERT CONTENT',
+      key: 'KEY CONTENT'
+    }
+  },
+  database: {
+    type: 'postgres',
+    host: 'app-postgres',
+    port: 5432,
+    username: 'app',
+    password: 'app',
+    database: 'app',
+    synchronize: true,
+    logging: true,
+    entities: [
+      'entity/**/*.entity.js'
+    ],
+    migrations: [],
+    subscribers: []
+  },
   router: {
+    static: { // Backend static html page
+      path: 'client'
+    },
+    version: 'v1',
     paths: PATH
+  },
+  session: {
+    domain: '',
+    name: 'session.id',
+    redis: {
+      host: 'app-redis',
+      port: 6379
+    }
   }
-});
+};
+
+/** Devlopment config. */
+const devConfig: KBSConfig = {
+  address: {
+    portocol: 'HTTP',
+    host: '0.0.0.0',
+    port: 8080,
+    proxy: true, // In proxy mode or not
+    ssl: {
+      cert: 'CERT CONTENT',
+      key: 'KEY CONTENT'
+    }
+  },
+  database: {
+    type: 'postgres',
+    host: 'app-postgres',
+    port: 5432,
+    username: 'app',
+    password: 'app',
+    database: 'app',
+    synchronize: true,
+    logging: true,
+    entities: [
+      'src/main/entity/**/*.entity.ts'
+    ],
+    migrations: [],
+    subscribers: []
+  },
+  router: {
+    static: { // Backend static html page
+      path: 'client'
+    },
+    version: 'v1',
+    paths: PATH
+  },
+  session: {
+    domain: '',
+    name: 'session.id',
+    redis: {
+      host: 'app-redis',
+      port: 6379
+    }
+  }
+};
+
+const server = new Server(devConfig);
 // // Use example statistic middleware
 // server.use(statistic);
 
