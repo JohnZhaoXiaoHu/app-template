7d17d5a817a963f26c618dded787fec3d7f60755
IInfinity <DevinDon@Foxmail.com>
2019-04-16 18:19:38 +0800

    fix(main): fix option

--- server/src/main/index.ts
+++ server/src/main/index.ts
@@ -1,9 +1,10 @@
-import { Server, ServerConfig } from '@iinfinity/rester';
+import { Option, Rester } from '@iinfinity/rester';
+import { Statistic } from './entity';
 import PATH from './router';
 import { statistic } from './ware';
 
-/** Production config. */
-const prodConfig: ServerConfig = {
+/** Option. */
+const option: Option = {
   address: {
     portocol: 'HTTP',
     host: '0.0.0.0',
@@ -24,7 +25,7 @@ const prodConfig: ServerConfig = {
     synchronize: true,
     logging: true,
     entities: [
-      'entity/**/*.entity.js'
+      Statistic
     ],
     migrations: [],
     subscribers: []
@@ -38,57 +39,16 @@ const prodConfig: ServerConfig = {
   session: {
     domain: '',
     name: 'session.id',
+    expire: 24 * 3600,
     redis: {
       host: 'app-redis',
       port: 6379
-    }
-  }
-};
-
-/** Devlopment config. */
-const devConfig: ServerConfig = {
-  address: {
-    portocol: 'HTTP',
-    host: '0.0.0.0',
-    port: 8080,
-    proxy: true, // In proxy mode or not
-    ssl: {
-      cert: 'CERT CONTENT',
-      key: 'KEY CONTENT'
-    }
-  },
-  database: {
-    type: 'postgres',
-    host: 'app-postgres',
-    port: 5432,
-    username: 'app',
-    password: 'app',
-    database: 'app',
-    synchronize: true,
-    logging: true,
-    entities: [
-      'src/main/entity/**/*.entity.ts'
-    ],
-    migrations: [],
-    subscribers: []
-  },
-  router: {
-    static: { // Backend static html page
-      path: 'client'
     },
-    paths: PATH
-  },
-  session: {
-    domain: '',
-    name: 'session.id',
-    redis: {
-      host: 'app-redis',
-      port: 6379
-    }
+    secert: ['rester', 'server']
   }
 };
 
-const server = new Server(devConfig);
+const server = new Rester(option);
 // Use example statistic middleware
 server.use({ statistic });
 
